a241200aa973009d52d50fca5238e1f358c33141
Sarah Lawrence <sarahlawrence1152@gmail.com>
2021-05-18 09:36:44 -0400

    initial commit

---
+++ Variables_2/src/com/qa/java/Runner.java
@@ -0,0 +1,32 @@
+package com.qa.java;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		// Use System.out.println() to print out Hello World.
+		System.out.println("Hello World");
+
+		// Create a variable of type String and print that out.
+		String string1 = "I have 3 cats!";
+		System.out.println(string1);
+
+		// Create an uninitialised int value and assign it a value using the = symbol
+		int x;
+		x = 10;
+		System.out.println(x);
+
+		// Create a float variable that has a value already initialised and print that
+		// out.
+		float myFloat = 11.2f;
+		System.out.println(myFloat);
+
+		// Print 2 Hello Worlds out using the println method.
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+
+		// Print 2 Hello Worlds out using the print method.
+		System.out.print("Hello World!");
+		System.out.print("Hello World!");
+	}
+}
