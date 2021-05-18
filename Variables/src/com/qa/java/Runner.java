48b748e5af76ea569a022c2a8f29853bdd5df152
Sarah Lawrence <sarahlawrence1152@gmail.com>
2021-05-18 09:33:44 -0400

    initial commit

---
+++ Variables/src/com/qa/java/Runner.java
@@ -0,0 +1,33 @@
+package com.qa.java;
+
+//Class has a scope
+public class Runner {
+
+	// Method has a scope
+	public static void main(String[] args) {
+
+		// Variables
+		// Strings
+		// char, boolean, int, short, long, float, double, byte
+
+		// Numeric? - int, short, float, double, long, byte
+		// Text? - Char, boolean, !string!
+
+		// declared variable
+		int numberVar;
+
+		numberVar = 12;
+
+		float myFloat = 12.5f;
+
+		String myString = "cat";
+		String myString2 = "dog";
+
+		myString2 = "the new rule";
+
+		System.out.println(numberVar);
+		System.out.println(myFloat);
+		System.out.println(myString2);
+		System.out.println("Hello World!");
+	}
+}
